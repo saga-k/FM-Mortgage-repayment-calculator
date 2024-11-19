c60e2246492963058d4863e7d77b4ac12e2a3fb5
saga-k <sagakrusell+1@gmail.com>
2024-11-19 15:35:11 +0100

    started ts

---
+++ mortgage-repayment-calculator-main/index.ts
@@ -0,0 +1,11 @@
+//Query selectors start --------------------------------------------------------
+
+const form = document.querySelector('form');
+const mortgageAmount = document.querySelector('#amountInput');
+const mortgageTerm = document.querySelector('#termInput');
+const repayment = document.querySelector('#repayment');
+const button = document.querySelector('button');
+const clearAll = document.querySelector('#clearAll');
+
+const emptyResults = document.querySelector('#emptyResults');
+const completedResults = document.querySelector('#completedResults');
