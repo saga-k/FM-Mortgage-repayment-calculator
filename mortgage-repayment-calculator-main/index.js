2ffe4be15b0b3822481c937741c04fd4c78966a5
saga-k <sagakrusell+1@gmail.com>
2024-11-20 10:36:34 +0100

    set up typescript compilation

---
+++ mortgage-repayment-calculator-main/index.js
@@ -0,0 +1,24 @@
+"use strict";
+//Query selectors start --------------------------------------------------------
+var form = document.querySelector('form');
+var mortgageAmount = document.querySelector('#amountInput');
+var mortgageTerm = document.querySelector('#termInput');
+var repayment = document.querySelector('#radioRepayment');
+var interestOnly = document.querySelector('#radioInterest');
+var button = document.querySelector('button');
+var clearAll = document.querySelector('#clearAll');
+var emptyResults = document.querySelector('#emptyResults');
+var completedResults = document.querySelector('#completedResults');
+var monthlyPayments = document.querySelector('#monthlyPaymentsNumber');
+var totalPayments = document.querySelector('#totalPaymentsNumber');
+console.log(repayment);
+//Styling functions start ------------------------------------------------------
+//mortgageTerm.addEventListener('focus', () => {
+//outline(mortgageTerm);
+//});
+//function outline(element) {
+//const parent = element.parent;
+//console.log(parent);
+//}
+//Styling functions end --------------------------------------------------------
+//# sourceMappingURL=index.js.map
--- mortgage-repayment-calculator-main/index.ts
+++ mortgage-repayment-calculator-main/index.ts
@@ -3,7 +3,10 @@
 const form = document.querySelector('form');
 const mortgageAmount = document.querySelector('#amountInput');
 const mortgageTerm = document.querySelector('#termInput');
-const repayment = document.querySelector('#repayment');
+
+const repayment = document.querySelector('#radioRepayment');
+const interestOnly = document.querySelector('#radioInterest');
+
 const button = document.querySelector('button');
 const clearAll = document.querySelector('#clearAll');
 
@@ -11,3 +14,18 @@ const emptyResults = document.querySelector('#emptyResults');
 const completedResults = document.querySelector('#completedResults');
 const monthlyPayments = document.querySelector('#monthlyPaymentsNumber');
 const totalPayments = document.querySelector('#totalPaymentsNumber');
+
+
+console.log(repayment);
+//Styling functions start ------------------------------------------------------
+//mortgageTerm.addEventListener('focus', () => {
+//outline(mortgageTerm);
+//});
+
+//function outline(element) {
+//const parent = element.parent;
+//console.log(parent);
+//}
+
+
+//Styling functions end --------------------------------------------------------
