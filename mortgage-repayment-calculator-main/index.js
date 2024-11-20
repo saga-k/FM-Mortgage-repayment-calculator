95d66d9e5ade33cf8f80075308d037490304844c
saga-k <sagakrusell+1@gmail.com>
2024-11-20 13:17:51 +0100

    radio states done

--- mortgage-repayment-calculator-main/index.js
+++ mortgage-repayment-calculator-main/index.js
@@ -1,24 +1,34 @@
 "use strict";
+//Colors
+var lime = 'hsl(61, 70%, 52%';
+var blue = 'hsl(200, 24%, 40%)';
 //Query selectors start --------------------------------------------------------
 var form = document.querySelector('form');
 var mortgageAmount = document.querySelector('#amountInput');
 var mortgageTerm = document.querySelector('#termInput');
-var repayment = document.querySelector('#radioRepayment');
-var interestOnly = document.querySelector('#radioInterest');
+var radioRepayment = document.querySelector('#radioRepayment');
+var labelRepayment = document.querySelector('#repaymentLabel');
+var interestOnlyRadio = document.querySelector('#radioInterestOnly');
+var interestOnlyLabel = document.querySelector('#interestOnlyLabel');
 var button = document.querySelector('button');
 var clearAll = document.querySelector('#clearAll');
 var emptyResults = document.querySelector('#emptyResults');
 var completedResults = document.querySelector('#completedResults');
 var monthlyPayments = document.querySelector('#monthlyPaymentsNumber');
 var totalPayments = document.querySelector('#totalPaymentsNumber');
-console.log(repayment);
+//console.log(radioRepayment);
 //Styling functions start ------------------------------------------------------
-//mortgageTerm.addEventListener('focus', () => {
-//outline(mortgageTerm);
-//});
-//function outline(element) {
-//const parent = element.parent;
-//console.log(parent);
-//}
+radioRepayment.addEventListener('focus', function () {
+    labelRepayment.style.borderColor = lime;
+});
+radioRepayment.addEventListener('blur', function () {
+    labelRepayment.style.borderColor = blue;
+});
+interestOnlyRadio.addEventListener('focus', function () {
+    interestOnlyLabel.style.borderColor = lime;
+});
+interestOnlyRadio.addEventListener('blur', function () {
+    interestOnlyLabel.style.borderColor = blue;
+});
 //Styling functions end --------------------------------------------------------
 //# sourceMappingURL=index.js.map
--- mortgage-repayment-calculator-main/index.ts
+++ mortgage-repayment-calculator-main/index.ts
@@ -1,31 +1,45 @@
+//Colors
+const lime = 'hsl(61, 70%, 52%';
+const blue = 'hsl(200, 24%, 40%)';
+
+
 //Query selectors start --------------------------------------------------------
 
-const form = document.querySelector('form');
-const mortgageAmount = document.querySelector('#amountInput');
-const mortgageTerm = document.querySelector('#termInput');
+const form = document.querySelector('form') as HTMLElement;
+const mortgageAmount = document.querySelector('#amountInput') as HTMLElement;
+const mortgageTerm = document.querySelector('#termInput') as HTMLElement;
 
-const repayment = document.querySelector('#radioRepayment');
-const interestOnly = document.querySelector('#radioInterest');
+const radioRepayment = document.querySelector('#radioRepayment') as HTMLElement;
+const labelRepayment = document.querySelector('#repaymentLabel') as HTMLElement;
 
-const button = document.querySelector('button');
-const clearAll = document.querySelector('#clearAll');
+const interestOnlyRadio = document.querySelector('#radioInterestOnly') as HTMLElement;
+const interestOnlyLabel = document.querySelector('#interestOnlyLabel') as HTMLElement;
 
-const emptyResults = document.querySelector('#emptyResults');
-const completedResults = document.querySelector('#completedResults');
-const monthlyPayments = document.querySelector('#monthlyPaymentsNumber');
-const totalPayments = document.querySelector('#totalPaymentsNumber');
+const button = document.querySelector('button') as HTMLElement;
+const clearAll = document.querySelector('#clearAll') as HTMLElement;
 
+const emptyResults = document.querySelector('#emptyResults') as HTMLElement;
+const completedResults = document.querySelector('#completedResults') as HTMLElement;
+const monthlyPayments = document.querySelector('#monthlyPaymentsNumber') as HTMLElement;
+const totalPayments = document.querySelector('#totalPaymentsNumber') as HTMLElement;
 
-console.log(repayment);
+//console.log(radioRepayment);
 //Styling functions start ------------------------------------------------------
-//mortgageTerm.addEventListener('focus', () => {
-//outline(mortgageTerm);
-//});
 
-//function outline(element) {
-//const parent = element.parent;
-//console.log(parent);
-//}
+radioRepayment.addEventListener('focus', () => {
+  labelRepayment.style.borderColor = lime;
+});
 
+radioRepayment.addEventListener('blur', () => {
+  labelRepayment.style.borderColor = blue;
+});
+
+interestOnlyRadio.addEventListener('focus', () => {
+  interestOnlyLabel.style.borderColor = lime;
+});
+
+interestOnlyRadio.addEventListener('blur', () => {
+  interestOnlyLabel.style.borderColor = blue;
+});
 
 //Styling functions end --------------------------------------------------------
